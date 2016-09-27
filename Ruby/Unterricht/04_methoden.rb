73e3e9bd4caf2124ba08656cbb8c6253280ed531
Tredar <mr.creps@freenet.de>
2016-09-27 09:05:47 +0200

    04_methoden.rb erstellt

---
+++ Ruby/Unterricht/04_methoden.rb
@@ -0,0 +1,9 @@
+def say_hello(name="Ludwig",health=100)
+
+	puts "Hallo ... Ich bin #{name} mit einem Wert von #{health} um #{set_time}"
+end
+
+def set_time
+	Time.now.strftime("%H:%M")
+end
+say_hello
