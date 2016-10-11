b24b8ccd2dd2b8377bcc4d7786a3f2c56594dec5
Tredar <mr.creps@freenet.de>
2016-10-11 12:50:06 +0200

    Dokumentation bei Arrays und Bloeke erweitert... Aufgabe 8 Game vervollständigt ..... Aufgabe 9 gestartet ...

--- Ruby/Unterricht/08_uebung_objects.rb
+++ Ruby/Unterricht/08_uebung_objects.rb
@@ -24,6 +24,62 @@ class Game
 		@players = []
 	end
 	def add_player(player)
-		@palyers << player
+		@players << player
+	end
+	def play
+		puts "There are #{@players.size} players in #{@title}:"
+		@players.each do |player|
+		  puts player
+		end
+
+		@players.each do |player|
+  			player.blam
+  			player.w00t
+  			player.w00t
+  			puts player
+		end
+
 	end
 end
+
+class Player
+
+  attr_accessor :name
+  attr_reader :health
+
+  def initialize(name, health=100)
+    @name = name.capitalize
+    @health = health
+  end
+  
+  def to_s
+    "I'm #{@name} with a health of #{@health} and a score of #{score}."
+  end
+
+  def blam
+    @health -= 10
+    puts "#{@name} got blammed!"
+  end
+  
+  def w00t
+    @health += 15
+    puts "#{@name} got w00ted!"
+  end
+  
+  def score
+    @health + @name.length
+  end
+  
+end
+
+player1 = Player.new("moe")
+player2 = Player.new("larry", 60)
+player3 = Player.new("curly", 125)
+
+players = [player1, player2, player3]
+
+game1 = Game.new("Knuckleheads")
+game1.add_player(player1)
+game1.add_player(player2)
+game1.add_player(player3)
+game1.play
